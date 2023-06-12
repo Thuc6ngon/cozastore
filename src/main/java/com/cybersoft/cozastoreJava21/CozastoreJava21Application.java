4178798ce76851de54c6df15aac2687b6c004812
Thuc6ngon <tranthuc04122003@gmail.com>
2023-06-13 00:36:04 +0700

    firstcommit

---
+++ src/main/java/com/cybersoft/cozastoreJava21/CozastoreJava21Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastoreJava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozastoreJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozastoreJava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/cozastoreJava21/CozastoreJava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastoreJava21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozastoreJava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
